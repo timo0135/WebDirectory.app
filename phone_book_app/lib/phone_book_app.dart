2943c3a8553edbeb6e827c3172fe8eb10770c189
Maxime Biechy <biechymaxime@gmail.com>
2024-06-14 11:34:27 +0200

    Fonctionnalités 2, 3, 6

--- phone_book_app/lib/phone_book_app.dart
+++ phone_book_app/lib/phone_book_app.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:phone_book_app/screens/entree_master.dart';
 
 class PhoneBookApp extends StatefulWidget {
@@ -13,6 +12,7 @@ class _PhoneBookAppState extends State<PhoneBookApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      // ! Titre de l'application
       title: 'Phone Book App',
       home: Scaffold(
         appBar: AppBar(
@@ -21,30 +21,13 @@ class _PhoneBookAppState extends State<PhoneBookApp> {
             'Phone Book App',
             style: TextStyle(color: Colors.white, fontSize: 18),
           ),
-          actions: <Widget>[
-            IconButton(
-              icon: const Icon(
-                Icons.search,
-                color: Colors.white,
-              ),
-              onPressed: () {
-                // Action à effectuer lors du clic sur l'icône de recherche
-              },
-            ),
-            Padding(
-              padding: const EdgeInsets.only(right: 10),
-              child: IconButton(
-                  icon: const Icon(
-                    Icons.sort_by_alpha,
-                    color: Colors.white,
-                  ),
-                  onPressed: () {}),
-            ),
-          ],
+          
+         
         ),
         body: const EntreeMaster(),
       ),
       debugShowCheckedModeBanner: false,
     );
   }
+  
 }
--- phone_book_app/lib/screens/entree_master.dart
+++ phone_book_app/lib/screens/entree_master.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:http/http.dart' as http;
 import 'package:phone_book_app/models/entree.dart';
 
@@ -11,7 +12,21 @@ class EntreeMaster extends StatefulWidget {
 }
 
 class _EntreeMasterState extends State<EntreeMaster> {
-  late Future<List<Entree>> entrees;
+  // ! Attributs
+  late Future<List<Entree>> entrees; // * Liste des entrées
+  String? dropdownvalue = 'Tous'; // * Valeur du menu déroulant
+  var items = [
+    'Tous',
+    'Ressources Humaines',
+    'Informatique',
+    'Marketing',
+    'Finance',
+    'Support Client'
+  ]; // * Éléments du menu déroulant
+
+  bool isAscending = true; // * Variable pour le tri des entrées
+  bool isSearching = false; // * Variable pour l'état de la recherche
+  TextEditingController searchController = TextEditingController();
 
   @override
   void initState() {
@@ -23,45 +38,128 @@ class _EntreeMasterState extends State<EntreeMaster> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: const Color.fromRGBO(239, 243, 247, 1),
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        leading: IconButton(
+          icon: const Icon(
+            Icons.menu,
+            color: Color.fromRGBO(3, 148, 222, 1),
+          ),
+          onPressed: () {},
+        ),
+        title: !isSearching
+            ? const Text(
+                '',
+                style: TextStyle(color: Color.fromRGBO(3, 148, 222, 1)),
+              )
+            : TextField(
+                controller: searchController,
+                style: const TextStyle(color: Color.fromRGBO(3, 148, 222, 1)),
+                decoration: const InputDecoration(
+                  hintText: 'Rechercher...',
+                  hintStyle: TextStyle(color: Color.fromRGBO(3, 148, 222, 1)),
+                  border: InputBorder.none,
+                ),
+                onChanged: (value) {
+                  setState(() {});
+                },
+              ),
+        actions: <Widget>[
+          // ! Bouton pour activer/désactiver la recherche
+          IconButton(
+            icon: Icon(
+              isSearching ? Icons.clear : Icons.search,
+              color: const Color.fromRGBO(3, 148, 222, 1),
+            ),
+            onPressed: () {
+              setState(() {
+                if (isSearching) {
+                  searchController.clear();
+                }
+                isSearching = !isSearching;
+              });
+            },
+          ),
+          // ! Menu déroulant pour filtrer par département
+          PopupMenuButton<String>(
+            icon: const Icon(
+              Icons.filter_list,
+              color: Color.fromRGBO(3, 148, 222, 1),
+            ),
+            onSelected: (String value) {
+              setState(() {
+                dropdownvalue = value;
+              });
+            },
+            itemBuilder: (BuildContext context) {
+              return items.map<PopupMenuItem<String>>((String value) {
+                return PopupMenuItem<String>(
+                  value: value,
+                  child: Text(value),
+                );
+              }).toList();
+            },
+          ),
+          // ! Bouton pour trier les entrées par nom (ascendant/descendant)
+          IconButton(
+            icon: const Icon(
+              Icons.sort_by_alpha,
+              color: Color.fromRGBO(3, 148, 222, 1),
+            ),
+            onPressed: () {
+              setState(() {
+                isAscending = !isAscending;
+              });
+            },
+          ),
+        ],
+      ),
       body: FutureBuilder<List<Entree>>(
         future: entrees,
         builder: (context, snapshot) {
           if (snapshot.hasData) {
-            // Tri des entrées par nom
-            snapshot.data!.sort((a, b) => a.nom.compareTo(b.nom));
+            // ! Filtrer des entrées par département et par recherche
+            List<Entree> filteredEntrees = _filterEntrees(snapshot.data!);
+            filteredEntrees =
+                _searchEntrees(filteredEntrees, searchController.text);
 
-            // Affichage des entrées
-            return ListView.builder(
-              itemCount: snapshot.data!.length,
-              itemBuilder: (context, index) {
-                final entree = snapshot.data![index];
-                return Padding(
-                  padding: const EdgeInsets.all(4.0),
-                  child: Container(
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(10),
-                      boxShadow: const [
-                        BoxShadow(
-                          color: Colors.white,
+            // ! Tri des entrées par nom
+            filteredEntrees.sort((a, b) =>
+                isAscending ? a.nom.compareTo(b.nom) : b.nom.compareTo(a.nom));
+
+            // ! Affichage des entrées
+            return Expanded(
+              child: ListView.builder(
+                itemCount: filteredEntrees.length,
+                itemBuilder: (context, index) {
+                  final entree = filteredEntrees[index];
+                  return Padding(
+                    padding: const EdgeInsets.all(4.0),
+                    child: Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                        boxShadow: const [
+                          BoxShadow(
+                            color: Colors.white,
+                          ),
+                        ],
+                      ),
+                      // ! Affichage des informations de l'entrée
+                      child: ListTile(
+                        leading: CircleAvatar(
+                          backgroundImage: NetworkImage(entree.image),
                         ),
-                      ],
-                    ),
-                    child: ListTile(
-                      leading: CircleAvatar(
-                        backgroundImage: NetworkImage(
-                            entree.image), // Chargement de l'image depuis l'URL
-                      ),
-                      title: Text('${entree.nom} ${entree.prenom}'),
-                      subtitle: Text(entree.departement.join(', ')),
-                      trailing: const Icon(
-                        Icons.phone_outlined,
-                        color: Color.fromRGBO(3, 148, 222, 1),
-                        
+                        title: Text('${entree.nom} ${entree.prenom}'),
+                        subtitle: Text(entree.departement.join(', ')),
+                        trailing: const Icon(
+                          Icons.phone_outlined,
+                          color: Color.fromRGBO(3, 148, 222, 1),
+                        ),
                       ),
                     ),
-                  ),
-                );
-              },
+                  );
+                },
+              ),
             );
           } else if (snapshot.hasError) {
             return Center(child: Text('ERREUR: ${snapshot.error}'));
@@ -72,7 +170,7 @@ class _EntreeMasterState extends State<EntreeMaster> {
     );
   }
 
-  // Récupération des entrées depuis l'API
+  // ! Récupération des entrées depuis l'API
   Future<List<Entree>> _fetchEntrees() async {
     final response = await http.get(
         Uri.parse('http://docketu.iutnc.univ-lorraine.fr:54002/api/entrees'));
@@ -85,6 +183,29 @@ class _EntreeMasterState extends State<EntreeMaster> {
       }).toList();
     } else {
       throw Exception('Impossible de récupérer les entrées');
+    }
+  }
+
+  // ! Filtrer des entrées par département
+  List<Entree> _filterEntrees(List<Entree> entrees) {
+    if (dropdownvalue == 'Tous') {
+      return entrees;
+    } else {
+      return entrees
+          .where((entree) => entree.departement.contains(dropdownvalue))
+          .toList();
+    }
+  }
+
+  // ! Filtrer des entrées par recherche
+  List<Entree> _searchEntrees(List<Entree> entrees, String query) {
+    if (query.isEmpty) {
+      return entrees;
+    } else {
+      return entrees.where((entree) {
+        return entree.nom.toLowerCase().contains(query.toLowerCase()) ||
+            entree.prenom.toLowerCase().contains(query.toLowerCase());
+      }).toList();
     }
   }
 }
